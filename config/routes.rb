54ca00f44969875c87b3e1b93a3b130813e75252
Alexandre Deniau <alexandre.deniau@outlook.fr>
2017-08-22 12:58:34 +0200

    ok routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'ebook', to: 'pages#ebook'
+  get 'start_now', to: 'pages#start_now'
+  get 'coaching', to: 'pages#coaching'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
