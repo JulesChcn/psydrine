2bfbbc3e64fe8cf7e0ae8d042cead35192d42000
Alexandre Deniau <alexandre.deniau@outlook.fr>
2017-08-24 14:15:19 +0200

    ok some stuff

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   get 'ebook', to: 'pages#ebook'
   get 'start_now', to: 'pages#start_now'
   get 'coaching', to: 'pages#coaching'
-  get 'index', to: 'posts#index'
-  get 'new', to: 'posts#new'
-  get 'edit', to: 'posts#edit'
-  get 'show', to: 'posts#show'
-  get 'delete', to: 'posts#delete'
+  # get 'index', to: 'posts#index'
+  # get 'new', to: 'posts#new'
+  # get 'edit', to: 'posts#edit'
+  # get 'show', to: 'posts#show'
+  # get 'delete', to: 'posts#delete'
   resources :posts
   # root to: "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
