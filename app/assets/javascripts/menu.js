7e0ea415715d9cd4a4df0e52071c6a0f6d0e15e4
Alexandre Deniau <alexandre.deniau@outlook.fr>
2017-08-29 16:56:48 +0200

    rien de tres nouveau

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -2,4 +2,26 @@ $(document).ready(function(){
   $('#nav-icon0,#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
     $(this).toggleClass('open');
   });
+//   $(document).ready(function(){
+
+//   $(".navbar-kachinah").hide();
+
+//   $(document).mousemove(function( event ) {
+//     $("navbar-kachinah").show();
+
+//     myStopFunction();
+//     myFunction();
+//   });
+
+//   function myFunction() {
+//     myVar = setTimeout(function(){
+//       $("navbar-kachinah").hide();
+//     }, 1000);
+//   }
+//   function myStopFunction() {
+//     if(typeof myVar != 'undefined'){
+//       clearTimeout(myVar);
+//     }
+//   }
+// });
 });
