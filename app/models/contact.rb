39cb127cf1a9fa4b2f3b36983e1f254bdd19ad9c
Alexandre Deniau <alexandre.deniau@outlook.fr>
2017-09-08 16:12:48 +0200

    ok mail on retst

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "My Contact Form",
-      :to => "alexandre.deniaul@outlook.fr",
+      :to => "alexandre.telemaque.deniau@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
