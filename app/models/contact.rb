09fd7cdd2dc5c4131e27bdf559b70d716aee8652
Alexandre Deniau <alexandre.deniau@outlook.fr>
2017-09-11 16:27:30 +0200

    oki bordel

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "My Contact Form",
-      :to => "your_email@example.org",
+      :to => "alexandre.deniau@outlook.com",
       :from => %("#{name}" <#{email}>)
     }
   end
